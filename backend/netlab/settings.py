3d34fc40e2dae193abff6d1177c57299b6a1ff98
saki <369512841@qq.com>
2017-03-26 10:30:06 +0800

    test

--- backend/netlab/settings.py
+++ backend/netlab/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
+    os.path.join(STATIC_ROOT, "js"),
 ]
