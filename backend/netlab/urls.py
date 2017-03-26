1227c7c419313104c420851abb936e0c2b9b41ec
saki <369512841@qq.com>
2017-03-26 10:50:23 +0800

    update

--- backend/netlab/urls.py
+++ backend/netlab/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
+from django.conf.urls.static import static
+from django.conf import settings
 from netlab import views
+
+
 
 urlpatterns = [
     url(r'^lab/$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
-]
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
